d7ca2718162ec680241fee0529ada6727c8531e2
lordzooshi <isaacmuini@gmail.com>
2023-02-16 10:34:23 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,20 @@
-/**
- * main - Entry point
- *
- * Description: This program prints the size of various types on the computer
- * it is compiled and run on.
- *
- * Return: Always 0 (success)
- */
-
 #include <stdio.h>
-#include <stdlib.h>
 
+/**
+ * main - prints the size of various types on the computer it is compiled and run on
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    printf("Size of a char: %ld byte(s)\n", sizeof(char));
-    printf("Size of an int: %ld byte(s)\n", sizeof(int));
-    printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-    printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-    printf("Size of a float: %ld byte(s)\n", sizeof(float));
-    printf("Size of a double: %ld byte(s)\n", sizeof(double));
-    printf("Size of a long double: %ld byte(s)\n", sizeof(long double));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a double: %lu byte(s)\n", sizeof(double));
+	printf("Size of a long double: %lu byte(s)\n", (unsigned long) sizeof(long double));
 
-    return (EXIT_SUCCESS);
+	return (0);
 }
 
